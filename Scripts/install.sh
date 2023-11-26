dff14a08e22f9bfadfd12974d43177d7b1bdf236
Sean Lord <sean.kenneth.lord@gmail.com>
2023-11-26 04:58:52 -0500

    first commit

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -81,21 +81,6 @@ EOF
         cat $cust_pkg >>install_pkg.lst
     fi
 
-    #--------------------------------#
-    # add nvidia drivers to the list #
-    #--------------------------------#
-    if nvidia_detect; then
-
-        cat /usr/lib/modules/*/pkgbase | while read krnl; do
-            echo "${krnl}-headers" >>install_pkg.lst
-        done
-
-        echo -e "nvidia-dkms\nnvidia-utils" >>install_pkg.lst
-        sed -i "s/^hyprland-git/hyprland-nvidia-git/g" install_pkg.lst
-
-    else
-        echo "nvidia card not detected, skipping nvidia drivers..."
-    fi
 
     #--------------------------------#
     # install packages from the list #
